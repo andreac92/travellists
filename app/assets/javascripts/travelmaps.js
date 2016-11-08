ef9db0400d2bb7d85b036cc1d242fe65c2103a78
Andrea Campos <andrea.ca92@yahoo.com>
2016-11-07 22:38:44 -0800

    adding place markers to map working

--- app/assets/javascripts/travelmaps.js
+++ app/assets/javascripts/travelmaps.js
@@ -12,37 +12,47 @@ travelMaps.init = (function () {
   }
 
   var addedPlaces = {};
+  var map;
 
   var continentCount = function(shortName){
     continentNum[shortName] += 1;
   }
 
   var getPlaces = function() {
-    var places = $("#list > p");
+    var places = $("#list > div");
     if (places.length){
       console.log("there are places!");
-      addedPlaces = places;
+      places.each(function (){
+        var id = $(this).attr('class');
+        var coords = JSON.parse($(this).children('span').text());
+        addPlacetoMap(id, coords);
+      });
+      console.log(addedPlaces);
     } else {
       console.log("no places...");
     }
   }
 
-  var addPlacestoMap = function() {
-    getPlaces();
-    if (addedPlaces) {
-
-    }
+  var addPlacetoMap = function(id, coords) {
+    var coords = {"lat":coords.lat, "lng":coords.lng};
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title: id
+    });
+    addedPlaces[id] = {coords: coords, marker: marker};
   }
 
   var initMap = function() {
       console.log("rendering...");
-      var map = new google.maps.Map(document.getElementById('map'), {
+      map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: {lat: 41.9, lng: 12.6},
         mapTypeControl: false,
         streetViewControl: false,
         panControl: false
       });
+      getPlaces();
      autocomplete = new google.maps.places.Autocomplete(
       (
           document.getElementById('place_name')), {
@@ -58,7 +68,7 @@ travelMaps.init = (function () {
         var last = place.address_components.length - 1;
         coords.short_name = (place.address_components)[last].short_name;
         console.log("Location: "+JSON.stringify(coords));
-        var coordInput = document.getElementById('coords');
+        var coordInput = document.getElementById('place_coords');
         coordInput.value = JSON.stringify(coords);
       }
      }
